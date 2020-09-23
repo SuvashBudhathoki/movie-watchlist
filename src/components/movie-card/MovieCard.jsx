5d155191f00e4c59aa9769fa629e505d490783d6
Suvash Budhathoki <suvashbudhathoki@gmail.com>
2020-09-23 13:30:28 +1000

    Total movies and style added to stats, image made clickable in watchlist

--- src/components/movie-card/MovieCard.jsx
+++ src/components/movie-card/MovieCard.jsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardImg, Row, Col } from 'reactstrap';
 import MovieModalCard from '../movie-detail-modal-card/MovieDetailModalCard';
 import { getPoster } from '../../utils/utils';
 import fillerImage from '../../fixtures/images/movie-card.jpg';
 
 const MovieCard = ({ movie }) => {
+
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
+
     return (
         <Row>
             <Col>
-                <Card>
+                <Card style={{ cursor: 'pointer' }}>
                     <CardImg
                         top
                         height='300rem'
                         width='100%'
                         src={getPoster(movie.Poster, fillerImage)}
                         alt='movie poster'
+                        onClick={toggle}
+
                     />
-                    <MovieModalCard movie={movie} />
+                    <MovieModalCard movie={movie} modal={modal} toggle={toggle} />
                 </Card>
             </Col>
-        </Row>
+        </Row >
     );
 };
 
